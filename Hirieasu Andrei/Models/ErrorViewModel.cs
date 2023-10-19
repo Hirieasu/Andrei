68c7f4996e0bacb124e5b40e5b6a0861056d600f
hirie <hirie@HIRI-PC>
2023-10-19 18:14:44 +0300

    Add project files.

---
+++ Hirieasu Andrei/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hirieasu_Andrei.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
